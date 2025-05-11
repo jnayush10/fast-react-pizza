42f0c93785e4053e470d380dc54623e556d19c45
Ayush Jain <jnayush10@gmail.com>
2025-05-12 00:11:13 +0530

    completed setup for tailwindcss

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,7 +7,7 @@ function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
+    <div>
       {isLoading && <Loader />}
       <Header />
       <main>
